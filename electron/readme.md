342c4f4e7ef7c0a3364a3d6af295cc2d02695164
Devout-Snail <flyants0077@outlook.com>
2020-05-13 20:30:15 +0800

    测试

--- electron/readme.md
+++ electron/readme.md
@@ -9,3 +9,7 @@ npm start
 
 ```
 Electron 文档[https://www.electronjs.org/docs/api]
+
+# electron
+
+运行起来寻找package.json的主要文件
--- sample/src/main.ts
+++ sample/src/main.ts
@@ -3,8 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { sayHello } from "./greet";
+import { mark } from "./performance"
 function showHello(divName: string, name: string) {
 	const elt = document.getElementById(divName);
 	elt.innerText = sayHello(name);
+	elt.innerText = mark("TEST");
 }
 showHello("greeting", "TypeScript");
